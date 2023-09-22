802bf05f247c9ecdd4ebae9e5f22befcedca7f44
bignemz <chinaemeemerole@yahoo.com>
2023-09-22 21:41:39 +0100

    update

--- app/src/Components/Introduction/Intro.js
+++ app/src/Components/Introduction/Intro.js
@@ -35,13 +35,226 @@ function Intro() {
     </section>
     <section className='intro2'>
         <div>
-            <div>
+            <div className='intro2-image'>
                 <img src="./HackImages/image2.png" alt=""/>
                
             </div>
         </div>
-         <div></div>
+         <div className='intro2-text'>
+            <h1>Introduction to getlinked
+<span>tech Hackathon 1.0 </span></h1>
+            <p>Our tech hackathon is a melting pot of visionaries, and its purpose is as clear as day: to shape the future. Whether you're a coding genius, a design maverick, or a concept wizard, you'll have the chance to transform your ideas into reality. Solving real-world problems, pushing the boundaries of technology, and creating solutions that can change the world, that's what we're all about!</p>
+         </div>
     </section>
+
+    <section className='intro3'>
+    
+        <div className='intro3-text'>
+            <h1>Introduction to getlinked
+            <span>tech Hackathon 1.0 </span></h1>
+            <p>Our tech hackathon is a melting pot of visionaries, and its purpose is as clear as day: to shape the future. Whether you're a coding genius, a design maverick, or a concept wizard, you'll have the chance to transform your ideas into reality. Solving real-world problems, pushing the boundaries of technology, and creating solutions that can change the world, that's what we're all about!</p>
+         </div>
+        
+       
+         <div className='intro3-image'>
+                <img src="./HackImages/image3.png" alt=""/>
+               
+            </div>
+    </section>
+
+    <section className='intro4'>
+    
+    <div className='intro4-image'>
+            <img src="./HackImages/image4.png" alt=""/>
+           
+        </div>
+    
+   
+        <div className='intro4-text'>
+            <h1>Introduction to getlinked
+            <span>tech Hackathon 1.0 </span></h1>
+            <p><span>Innovation and Creativity:</span> Evaluate the uniqueness and creativity of the solution. Consider whether it addresses a real-world problem in a novel way or introduces innovative features.</p>
+            <p><span>Functionality:</span> Assess how well the solution works. Does it perform its intended functions effectively and without major issues? Judges would consider the completeness and robustness of the solution.</p>
+            <p><span>Impact and Relevance: </span>Determine the potential impact of the solution in the real world. Does it address a significant problem, and is it relevant to the target audience? Judges would assess the potential social, economic, or environmental benefits.</p>
+            <p><span>Impact and Relevance: </span>Determine the potential impact of the solution in the real world. Does it address a significant problem, and is it relevant to the target audience? Judges would assess the potential social, economic, or environmental benefits.</p>
+            <p><span>Technical Complexity: </span>Evaluate the technical sophistication of the solution. Judges would consider the complexity of the code, the use of advanced technologies or algorithms, and the scalability of the solution.</p>
+            <p><span>Adherence to Hackathon Rules:</span> Judges will Ensure that the team adhered to the rules and guidelines of the hackathon, including deadlines, use of specific technologies or APIs, and any other competition-specific requirements.</p>
+
+            <button className='register'>Register</button>
+            
+         </div>
+    </section>
+    <section className='intro5'>
+    
+    <div className='intro5-text'>
+        <h1>Frequently Ask<span>Question </span></h1>
+        <p className='text1'>We got answers to the questions that you might want to ask about getlinked Hackathon 1.0!</p>
+
+        <div className='intro5-quest'>
+        <p>Can I work on a project I started before the hackathon?<span>+</span> </p>
+        </div>
+        <div className='intro5-quest'>
+        <p>What happens if I need help during the hackathon?<span>+</span> </p>
+        </div>
+        <div className='intro5-quest'>
+        <p>What happens if I don't have an idea for a project?<span>+</span></p>
+        </div>
+        <div className='intro5-quest'>
+        <p>Can I join a team or do I have to come with one?<span>+</span> </p>
+        </div>
+        <div className='intro5-quest'>
+        <p>What happens after the hackathon ends<span>+</span></p>
+        </div>
+        <div className='intro5-quest'>
+        <p>Can I work on a project I started before the hackathon?<span>+</span></p>
+        </div>
+        
+
+
+     </div>
+    
+   
+     <div className='intro5-image'>
+            <img src="./HackImages/image6.png" alt=""/>
+           
+        </div>
+</section>
+
+<section className='intro6'>
+    
+    <div className='intro6-text'>
+        <h1>Timeline</h1>
+        <p>Here is the breakdown of the time we anticipate using for the upcoming event.</p>
+     </div>
+    
+   
+     <div className='intro6-para'>
+        <div className='intro6-para1'>
+            <div className='intro6-header'>
+                <h2><span>Hackathon Announcement</span></h2>
+                <p>The getlinked tech hackathon 1.0 is formally announcedto the general public and teams begin to get ready to register</p>
+            </div>
+            <div className='intro6-mid one'><h3>1</h3></div>
+            <div className='intro6-tail'><h2><span>November 18, 2023</span></h2></div>
+        </div>
+        <div className='intro6-para1'>
+        <div className='intro6-header'>
+        <h2><span>November 18, 2023</span></h2>
+            </div>
+            <div className='intro6-mid two'><h3>2</h3></div>
+            <div className='intro6-tail'><h2><span>Teams Registration begins</span></h2>
+                <p>Interested teams can now show their interest in the getlinked tech hackathon 1.0 2023 by proceeding to register</p></div>
+        </div>
+        <div className='intro6-para1'>
+        <div className='intro6-header'>
+                <h2><span>Teams Registration ends</span></h2>
+                <p>Interested Participants are no longer Allowed to register</p>
+            </div>
+            <div className='intro6-mid three'><h3>3</h3></div>
+            <div className='intro6-tail'><h2><span>November 18, 2023</span></h2></div>
+        </div>
+        <div className='intro6-para1'>
+        <div>
+        <h2 className='intro6-header'><span>November 18, 2023</span></h2>
+            </div>
+            <div className='intro6-mid four' ><h3>4</h3></div>
+            <div className='intro6-tail'><h2><span>Announcement of the accepted teams and ideas</span></h2>
+                <p>All teams whom idea has been accepted into getlinked tech hackathon 1.0 2023 are formally announced</p></div>
+        </div>
+        <div className='intro6-para1'>
+        <div className='intro6-header'>
+                <h2><span>Getlinked Hackathon 1.0 Offically Beginst</span></h2>
+                <p>Accepted teams can now proceed to build their  ground breaking skill driven solutions</p>
+            </div>
+            <div className='intro6-mid five'><h3>5</h3></div>
+            <div className='intro6-tail'><h2><span>November 18, 2023</span></h2></div>
+        </div>
+        <div className='intro6-para1'>
+        <div className='intro6-header'>
+        <h2><span>November 18, 2023</span></h2>
+            </div>
+            <div className='intro6-mid' ><h3>6</h3></div>
+            <div className='intro6-tail'><h2><span>Demo Day</span></h2>
+                <p>Teams get the opportunity to pitch their projects to judges.The winner of the hackathon will also be announced on this day</p></div>
+        </div>
+            
+           
+        </div>
+</section>
+<section className='intro7'>
+        <div>
+            <div className='intro7-image'>
+                <img src="./HackImages/image7.png" alt=""/>
+               
+            </div>
+        </div>
+         <div className='intro7-text'>
+           <div className='intro7-prize'> <h1>Prizes and <span>Rewards </span></h1>
+            <p>Rewards</p></div>
+
+            <div className='intro7-medal'>
+             <div  className='intro7-medal1 silver' >
+                <img src="./HackImages/image8.png" alt=""/>
+                <h1>2nd</h1>
+                <h3>Runner</h3>
+                <h1>N300,000</h1>
+                </div>
+               
+                 <div className='intro7-medal1 gold' >
+                <img src="./HackImages/image10.png" alt=""/>
+                <h1>1st</h1>
+                <h3>Runner</h3>
+                <h1>N300,000</h1>
+                </div>
+             <div className='intro7-medal1 bronze' >
+                <img src="./HackImages/image9.png" alt=""/>
+                <h1>3rd</h1>
+                <h3>Runner</h3>
+                <h1>N150,000</h1>
+                </div>
+               </div> 
+         </div>
+    </section>
+    <section className='intro8'>
+    
+    <div className='intro8-text'>
+        <h1>Partners and Sponsors</h1>
+        <p>Getlinked Hackathon 1.0 is honored to have the following major 
+companies as its partners and sponsors</p>
+     </div>
+    
+   
+     <div className='intro8-para'>
+        <div className='intro8-para1'>
+            
+        <img src="./HackImages/company1.png" alt=""/>
+           
+        </div>
+        <div className='intro8-para1'>
+        <img src="./HackImages/company2.png" alt=""/>
+        
+        </div>
+        <div className='intro8-para1'>
+        <img src="./HackImages/company3.png" alt=""/>
+       
+        </div>
+        <div className='intro8-para1'>
+        <img src="./HackImages/company4.png" alt=""/>
+       
+        </div>
+        <div className='intro8-para1'>
+        <img src="./HackImages/company5.png" alt=""/>
+     
+        </div>
+        <div className='intro8-para1'>
+        <img src="./HackImages/company6.png" alt=""/>
+       
+        </div>
+            
+           
+        </div>
+</section>
+
     </div>
 
     
--- app/src/Components/Navbar/Navbar.js
+++ app/src/Components/Navbar/Navbar.js
@@ -28,7 +28,6 @@ function Navbar() {
         </div> 
 
 
-
        
     </nav>
   )
